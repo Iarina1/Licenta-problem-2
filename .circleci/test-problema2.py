f65647bf76c8c560a4e457ce8a4beb281622eeab
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-06-12 21:22:19 +0300

    Update test-problema2.py

--- .circleci/test-problema2.py
+++ .circleci/test-problema2.py
@@ -15,13 +15,9 @@ string1 = 'led_pin=5'
 string2 =  'digitalWrite(led_pin,HIGH)'
 
 if check_problema2(problema2, string1):
-    print("Test 1 passed")
     if check_problema2(problema2, string2):
-      print("Test 2 passed")
       print("Punctaj total: 10")
     else:
-      print("Test 2 failed")
       print("Punctaj total: 5")
 else:
-    print("Test 1 failed")
     print("Punctaj total: 0")
