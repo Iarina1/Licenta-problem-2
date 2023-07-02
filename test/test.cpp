8d270ce7c487c7e1cf1c73cc24155678e0954e04
Iarina Dalimon <49359405+Iarina1@users.noreply.github.com>
2023-07-02 16:55:33 +0300

    Create test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <ArduinoUnitTests.h>
+#include "Led.h"
+
+#define LED_BUILTIN1 13
+#define LED_BULTIN2 12
+#DEFINE LED_BUILTIN3 11
+
+unittest(blink)
+{
+    Led led;
+    GodmodeState* state = GODMODE();
+    bool values1[2];
+    bool values2[2];
+    bool values3[2];
+    unsigned long start;
+    start = micros();
+    assertEqual(0, start);
+    state->reset(); // clock and pins
+    assertEqual(1, state->digitalPin[LED_BUILTIN1].historySize());
+    assertEqual(1, state->digitalPin[LED_BUILTIN2].historySize());
+    assertEqual(1, state->digitalPin[LED_BUILTIN3].historySize());
+    led.setup();
+    assertEqual(1, state->digitalPin[LED_BUILTIN1].historySize());
+    assertEqual(1, state->digitalPin[LED_BUILTIN2].historySize());
+    assertEqual(1, state->digitalPin[LED_BUILTIN3].historySize());
+    led.loop();
+    assertEqual(2, state->digitalPin[LED_BUILTIN1].historySize());
+    assertEqual(2, state->digitalPin[LED_BUILTIN2].historySize());
+    assertEqual(2, state->digitalPin[LED_BUILTIN3].historySize());
+    assertEqual(1, values1[1]);
+    assertEqual(1, values2[1]);
+    assertEqual(1, values3[1]);
+    
+}
+
+unittest_main()
